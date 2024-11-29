8cd8e42fc559bc6ad7b1ce28597636b390bc9fcf
Farhan Alam <115414440+Istaqalam@users.noreply.github.com>
2024-11-29 16:31:32 +0545

    Add:Backend Foundation Files

---
+++ backend/api/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import College, Note, Event, QuestionPaper
+
+admin.site.register(College)
+admin.site.register(Note)
+admin.site.register(Event)
+admin.site.register(QuestionPaper)
