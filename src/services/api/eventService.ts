6a1a14df4e6245a73958a392b8fa4e408373420c
Safal Lama <140276215+RyuZinOh@users.noreply.github.com>
2024-12-06 21:46:21 +0545

    Delete src directory

--- src/services/api/eventService.ts
+++
@@ -1,8 +0,0 @@
-import api from '../config/axios';
-import { safeRequest } from '../utils/apiUtils';
-import { Event } from '../types';
-
-export const eventService = {
-  getAll: () => safeRequest<Event[]>(() => api.get('/events/')),
-  register: (id: string) => safeRequest<void>(() => api.post(`/events/${id}/register/`)),
-};
