83e29500192f8d806fab91d32142e48abc11ce81
Safal Lama <140276215+RyuZinOh@users.noreply.github.com>
2024-12-06 21:51:18 +0545

    merging src (#4)
    * Delete backend directory
    
    * Delete get-pip.py
    
    * Delete manage.py
    
    * Delete src directory
    
    * Add files via upload

--- backend/api/services/question_paper_service.py
+++
@@ -1,9 +0,0 @@
-from ..models import QuestionPaper
-
-class QuestionPaperService:
-    @staticmethod
-    def get_papers_by_year_and_semester(year, semester):
-        return QuestionPaper.objects.filter(
-            year=year,
-            semester=semester
-        )
