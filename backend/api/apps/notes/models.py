6ad0dd4b22a316055de36d1751f40d0aa6870fdc
Farhan Alam <115414440+FarhanAlam-Official@users.noreply.github.com>
2025-04-13 15:31:54 +0545

    refactor: update notes models and migrations

--- backend/api/apps/notes/models.py
+++ backend/api/apps/notes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
-from api.apps.subjects.models import Subject
+from api.apps.question_papers.models import Subject
 
 class Note(models.Model):
     SEMESTER_CHOICES = [
